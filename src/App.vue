3bc3cd7ab0e7fbc00d42a8522730e824bfee63ba
luxass <lucasnrgaard@gmail.com>
2023-09-16 12:33:01 +0200

    fix: added underlines to links

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,10 @@
     Well, it's a file that are used by <a
       href="https://github.com/luxass/luxass.dev/blob/main/scripts/update-site.ts"
       target="_blank" rel="noopener noreferrer"
+      class="underline underline-offset-5 underline-blue-600"
     >my website</a> to generate a list of projects that are being showed <a
       href="https://luxass.dev/projects" target="_blank" rel="noopener noreferrer"
+      class="underline underline-offset-5 underline-blue-600"
     >here</a>.
   </p>
 </template>
