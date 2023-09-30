b028af08fd8cd8ce730bb64745270155ab464693
luxass <lucasnrgaard@gmail.com>
2023-09-30 13:41:22 +0200

    fix: correct error page

--- error.vue
+++ error.vue
@@ -1,3 +1,13 @@
 <template>
-  <div />
+  <div class="h-100dvh flex gap-4 flex-col items-center justify-center">
+    <h1 class="text-8xl text-blue-500">
+      404
+    </h1>
+    <p class="mb-8 text-2xl">
+      Page not found
+    </p>
+    <Link href="/">
+      Go back
+    </Link>
+  </div>
 </template>
