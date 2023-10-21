f0e0fe01c5a096da0033569638f651a46dfde449
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-10-21 08:08:40 +0200

    chore: testing rewrites

--- plugins/middleware.ts
+++ plugins/middleware.ts
@@ -1,8 +1,8 @@
 // i couln't get the middleware to work without this.. Probably a bug or im just dumb.
 export default defineNuxtPlugin(() => {
-  addRouteMiddleware("*", (to) => {
-    if (to.path === "/schema" || to.path === "/schema.json") {
-      return navigateTo("/api/schema", { replace: false });
-    }
-  }, { global: true });
+  // addRouteMiddleware("*", (to) => {
+  //   if (to.path === "/schema" || to.path === "/schema.json") {
+  //     return navigateTo("/api/schema", { replace: false });
+  //   }
+  // }, { global: true });
 });
