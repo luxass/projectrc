44f59bbdaca0a4faa6808bc1a79c046965c65c20
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-04 16:44:40 +0100

    chore(projectrc): testing

--- packages/projectrc/src/schema.ts
+++ packages/projectrc/src/schema.ts
@@ -1,8 +1,27 @@
-import { boolean, object, optional, string, union } from "valibot";
+import { array, boolean, object, optional, startsWith, string, union, url } from "valibot";
 
 export const SCHEMA = object({
   readme: optional(union([
     boolean(),
-    string(),
+    optional(string(), "README.md"),
   ]), false),
+  npm: optional(union([
+    boolean(),
+    optional(string(), "package.json"),
+  ]), false),
+
+  ignore: optional(boolean(), false),
+
+  website: optional(union([
+    boolean(),
+    string([url()]),
+  ]), false),
+
+  handles: optional(array(
+    string([
+      startsWith("/"),
+    ]),
+  ), []),
+
+  monorepo: optional(boolean(), false),
 });
