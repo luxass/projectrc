5f180262ea19dd5f807ddbb5f687a64ce58e6e1f
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-04 13:05:26 +0100

    ci: added turbo remote caching

--- packages/projectrc/tsup.config.ts
+++ packages/projectrc/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(
   {
-    entry: ["./src/index.ts"],
+    entry: ["./src/index.ts", "./src/schema.ts"],
     format: ["cjs", "esm"],
     clean: true,
     dts: true,
