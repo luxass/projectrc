227fd0da45227ca821eacfec6f1d61e40a9ed321
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-04 14:20:53 +0100

    feat(projectrc): extract schema into package

---
+++ packages/projectrc/src/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG_FILE_NAMES: string[] = [
+  ".projectrc",
+  ".projectrc.json",
+  ".projectrc.json5",
+];
--- packages/projectrc/src/index.ts
+++ packages/projectrc/src/index.ts
@@ -1,3 +1,5 @@
-export function hello() {
-  console.info("hello");
-}
+export const PROJECTRC_NAMES: string[] = [
+  ".projectrc", ".projectrc.json",
+];
+export const ALLOWED_OWNERS: string[] = ["luxass"];
+export const BLOCKED_REPOSITORIES: string[] = [];
