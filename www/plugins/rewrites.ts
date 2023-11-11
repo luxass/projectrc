bc06502764a60374fac3dcb28a5a1715144f9f29
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-11 11:41:58 +0100

    fix: redirect not working

--- www/plugins/rewrites.ts
+++ www/plugins/rewrites.ts
@@ -1,8 +1,9 @@
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   addRouteMiddleware("*", (to) => {
     if (to.path === "/view-source") {
       return navigateTo("https://github.com/luxass/projectrc", { replace: false });
     }
+
     if (to.path === "/schema" || to.path === "/schema.json") {
       return navigateTo("/api/schema", { replace: true });
     }
