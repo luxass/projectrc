8aa657df5f8f4ee2ecc2999e076dac317d3a2512
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-07 16:37:42 +0100

    does this work?

--- env.mjs
+++ env.mjs
@@ -5,7 +5,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   GITHUB_TOKEN: z.string(),
-  VERCEL_URL: z.string(),
+  VERCEL_URL: z.string().optional(),
 });
 
 export const env = envSchema.parse(process.env);
