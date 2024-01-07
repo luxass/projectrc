3e224ad4aa777d2b88d6961a2f803fc69ff69b31
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-07 21:15:42 +0100

    does this work?

--- app/projects/revalidate/route.ts
+++ app/projects/revalidate/route.ts
@@ -2,6 +2,7 @@ import { revalidatePath } from "next/cache";
 
 export async function GET() {
   revalidatePath("/projects");
+  revalidatePath("/resolve/[owner]/[repository]");
 
   return Response.json({
     message: "revalidated /projects",
