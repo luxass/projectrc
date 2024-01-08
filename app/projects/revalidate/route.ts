00741a82fa7a1595a5dd02b17c14869d89fa0eec
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-08 17:40:57 +0100

    fix: add revalidation token

--- app/projects/revalidate/route.ts
+++ app/projects/revalidate/route.ts
@@ -1,6 +1,16 @@
 import { revalidatePath } from "next/cache";
+import { env } from "~/env.mjs";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${env.REVALIDATE_TOKEN}`) {
+    return new Response(JSON.stringify({
+      error: "not authorized",
+    }), {
+      status: 403,
+    });
+  }
+
   revalidatePath("/projects");
   revalidatePath("/resolve/[owner]/[repository]");
 
--- env.mjs
+++ env.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
 export const envSchema = z.object({
   GITHUB_TOKEN: z.string(),
   VERCEL_URL: z.string().optional(),
+  REVALIDATE_TOKEN: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
