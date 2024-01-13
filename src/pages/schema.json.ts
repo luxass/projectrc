f58f659e4e36b3c2c1002efbc6c8c72ec3597aaa
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-13 13:14:51 +0100

    feat: add cache headers

--- src/pages/schema.json.ts
+++ src/pages/schema.json.ts
@@ -6,10 +6,7 @@ export const GET: APIRoute = () => {
   const jsonSchema = zodToJsonSchema(PROJECTRC_SCHEMA);
   return new Response(JSON.stringify(jsonSchema), {
     headers: {
-      "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "max-age=3600",
-      "Content-Disposition": "inline ",
     },
   });
 };
