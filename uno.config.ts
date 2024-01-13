c28008b270ec0a3e0d47da471d67bb0637ef083e
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-13 13:04:32 +0100

    fix: center text on 404 page

--- uno.config.ts
+++ uno.config.ts
@@ -9,19 +9,11 @@ import {
 
 export default defineConfig({
   presets: [
-    presetUno({
-      dark: "media",
-    }),
+    presetUno(),
     presetIcons({
       scale: 1.3,
     }),
     presetTypography(),
   ],
-  // shortcuts: {
-  //   "bg-active": "bg-gray:10",
-  //   "bg-base": "bg-white dark:bg-[#151515]",
-  //   "bg-secondary": "bg-gray:5",
-  //   "border-base": "border-gray/20",
-  // },
   transformers: [transformerDirectives(), transformerVariantGroup()],
 });
