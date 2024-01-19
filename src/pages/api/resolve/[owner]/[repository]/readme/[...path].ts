71a130559aceb738812e1ceefa9724e9f022b2f3
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-19 12:33:33 +0100

    feat: move mdx endpoint into readme endpoint

--- src/pages/api/resolve/[owner]/[repository]/readme/[...path].ts
+++ src/pages/api/resolve/[owner]/[repository]/readme/[...path].ts
@@ -1,9 +1,13 @@
 import type { APIRoute } from "astro";
+import { remark } from "remark";
 import { getREADME } from "~/lib/readme";
+import { removeBadges } from "~/lib/remark-plugins/badge-remover";
+import { removeComments } from "~/lib/remark-plugins/remove-comments";
+import { rewriteUrls } from "~/lib/remark-plugins/url-rewriter";
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ params }) => {
+export const GET: APIRoute = async ({ params, request }) => {
   const { owner, repository, path } = params;
   if (!owner || !repository) {
     return new Response("missing params", {
@@ -21,6 +25,34 @@ export const GET: APIRoute = async ({ params }) => {
     return new Response("not found", {
       status: 404,
     });
+  }
+
+  const xMDX = request.headers.get("X-MDX");
+
+  if (xMDX) {
+    const file = await remark()
+      .use(rewriteUrls({
+        repoUrl: `https://github.com/${owner}/${repository}`,
+      }))
+      .use(removeBadges())
+      .use(removeComments())
+      .process(readme.content || "No README was found.");
+
+    const mdx = file.toString();
+
+    return Response.json(
+      {
+        lastModified: new Date().toISOString(),
+        path: readme?.path,
+        content: mdx,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Cache-Control": "public, s-maxage=3600, must-revalidate",
+        },
+      },
+    );
   }
 
   return Response.json(
