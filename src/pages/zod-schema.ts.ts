bf56d6016d7d55bff823a4f241b5567d1c41dbd7
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-27 12:17:13 +0100

    Update zod-schema.ts.ts

--- src/pages/zod-schema.ts.ts
+++ src/pages/zod-schema.ts.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import ZOD_SCHEMA from "../lib/schema?raw";
+import ZOD_SCHEMA from "../lib/json-schema?raw";
 
 export const GET: APIRoute = () => {
   return new Response(ZOD_SCHEMA, {
