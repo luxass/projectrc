d82085473a18e514dcd234c6d033e7a996675c15
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-30 10:30:17 +0200

    fix: add `$schema` to schema

--- src/pages/schema.json.ts
+++ src/pages/schema.json.ts
@@ -1,9 +1,14 @@
 import type { APIRoute } from "astro";
+import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import { PROJECTRC_SCHEMA } from "~/lib/json-schema";
 
 export const GET: APIRoute = () => {
-  const jsonSchema = zodToJsonSchema(PROJECTRC_SCHEMA);
+  const jsonSchema = zodToJsonSchema(PROJECTRC_SCHEMA.merge(z.object({
+    $schema: z.string({
+      description: "Ignored. Can be set to get completions, validations and documentation in some editors.",
+    }).default("https://projectrc.luxass.dev/schema.json"),
+  })));
   return new Response(JSON.stringify(jsonSchema), {
     headers: {
       "Access-Control-Allow-Origin": "*",
