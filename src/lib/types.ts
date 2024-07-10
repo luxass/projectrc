2cc842491eae4c4b07db07270ab5aaeb5f8caf62
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-10 07:33:12 +0200

    chore: fix type errors

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,44 +1,10 @@
 import type { z } from "zod";
 import type { Language, Repository } from "github-schema";
-import type { PROJECTRC_SCHEMA } from "./json-schema";
+import type { MOSAIC_SCHEMA } from "./json-schema";
 
-type SafeOmit<T, K extends keyof T> = Omit<T, K>;
+// type SafeOmit<T, K extends keyof T> = Omit<T, K>;
 
-export type ResolvedProject = SafeOmit<
-  z.infer<typeof PROJECTRC_SCHEMA>,
-  "readme" | "workspace" | "stars" | "npm" | "version"
-> & {
-  /**
-   * The name of the project
-   */
-  name: string;
-
-  /**
-   * URL to the readme file
-   *
-   * NOTE:
-   * We are not including the full readme in the response,
-   * due to some readme files being very large.
-   */
-  readme?: string;
-
-  /**
-   * The number of stars the repository has
-   */
-  stars?: number;
-
-  /**
-   * The npm configuration
-   */
-  npm?: SafeOmit<Exclude<NonNullable<z.infer<typeof PROJECTRC_SCHEMA>["npm"]>, boolean>, "enabled"> & {
-    url?: string;
-  };
-
-  /**
-   * The version of the project
-   */
-  version?: string;
-};
+export type ResolvedProject = z.infer<typeof MOSAIC_SCHEMA>;
 
 export type Project = ResolvedProject &
   Pick<Repository, "nameWithOwner" | "pushedAt" | "url"> & {
